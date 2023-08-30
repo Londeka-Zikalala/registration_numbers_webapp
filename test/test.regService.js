0a3341c7e327a33d2fbf6ae905d165786e2870dc
Londeka Zikalala <londekazikalala@gmail.com>
2023-08-30 15:45:12 +0200

    test

--- test/test.regService.js
+++ test/test.regService.js
@@ -4,32 +4,36 @@ import db from '../db.js'
 
 
 describe('The regService tests', function(){
+    this.timeout(6000); 
     let registrationService = regService(db)
+
     beforeEach(async function () {
-        try {
+        this.timeout(5000); 
             // clean the tables before each test run
             await db.none("TRUNCATE TABLE registrations.towns RESTART IDENTITY CASCADE;");
             await db.none("TRUNCATE TABLE registrations.reg_numbers RESTART IDENTITY CASCADE;");
-        } catch (err) {
-            console.log(err);
-            throw err;
-        }
-        after(function () {
-            db.$pool.end;})
-
+        //Insert a town
+        await db.none("INSERT INTO registrations.towns (town_name) VALUES ('Johannesburg');");
+         
+      })
+      
+        
         it('should add a new registration',async function(){
             var reg ={
-                regNumber: 'CY 623 428',
+            regNumber: 'CY 623 428',
                 townId: 1
             };
            await  registrationService.addReg(reg)
 
            var getRegistration = await registrationService.getReg()
 
-           assert.strictEqual(getRegistration.length, 1)
-           assert.strictEqual(getRegistration[0].regNumber, 'CY 623 428')
+           assert.equal(getRegistration.length, 1)
+           assert.equal(getRegistration[0].reg_number, 'CY 623 428')
         })
+
+        after(function () {
+            db.$pool.end;})
+
     });
 
    
-})
