83120b9b6b1edde953b2347cdc6b895d745e57ec
Londeka Zikalala <londekazikalala@gmail.com>
2023-09-01 17:22:35 +0200

    timeout decreased

--- test/test.regService.js
+++ test/test.regService.js
@@ -4,11 +4,11 @@ import db from '../db.js'
 
 
 describe('The regService tests', function(){
-    this.timeout(15000); 
+    this.timeout(4000); 
     let registrationService = regService(db)
 
     beforeEach(async function () {
-        this.timeout(10000); 
+        this.timeout(4000); 
             // clean the tables before each test run
             await db.none("TRUNCATE TABLE registrations.towns RESTART IDENTITY CASCADE;");
             await db.none("TRUNCATE TABLE registrations.reg_numbers RESTART IDENTITY CASCADE;");
